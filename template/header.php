336f08eb31fe3448540a433fba2b6fee03b81a70
sudeep777 <sudeep777@gmail.com>
2018-03-12 22:14:24 -0400

    Update header.php

--- template/header.php
+++ template/header.php
@@ -1,8 +1,8 @@
                         <header class="container">
 							<div class="row">
 								<div class="col-xs-12 col-sm-4">
-									<a href="https://www.tracktik.com/">
-										<img class="header-logo" src="https://www.tracktik.com/wp-content/themes/tracktik/assets/images/ui/logo_black.svg" alt="TrackTik">
+									<a href="#">
+										<img class="header-logo" src="" alt="test">
 									</a>
 								</div>
 							
